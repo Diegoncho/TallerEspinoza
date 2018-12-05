a6b60a68e655774c330b28a7b979351365ce4387
Diego <Diego_escorchi@hotmail.com>
2018-12-05 10:48:43 -0600

    Reescrito

--- resources/views/CrudEmpleados/EmpleadoAdd.blade.php
+++ resources/views/CrudEmpleados/EmpleadoAdd.blade.php
@@ -4,7 +4,7 @@
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
-                <div class="panel-heading">Insertar Pene</div>
+                <div class="panel-heading">Insertar Empleado</div>
 
                     <div class="panel-body">
                         <form class="form-horizontal" action="" method="POST">
