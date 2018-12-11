f725fb5bd58ce176ab11f055d049054496c5d0e8
Diego <Diego_escorchi@hotmail.com>
2018-12-11 15:25:18 -0600

    Agregando Rutas para el Crud de "Clientes"

--- routes/web.php
+++ routes/web.php
@@ -31,7 +31,7 @@ Route::get('menu', 'MenuController@index')->name('menu');
 
 /*---------------------------------------*/
 
-
+//CRUD DE EMPLEADOS//
 /*---------------------------------------*/
 /* Listar Empleados */
 Route::get('/empleado', function(){
@@ -48,3 +48,24 @@ Route::get('empleadoAdd', 'EmpleadoController@create')->name('empleadoAdd');
 
 /* Agregar Empleado */
 Route::post('/empleadoAdd', 'EmpleadoController@post');
+/*---------------------------------------*/
+
+
+//CRUD DE CLIENTES//
+/*---------------------------------------*/
+/* Listar Clientes */
+Route::get('/cliente', function(){
+    return view('cliente');
+});
+Route::get('cliente', 'ClienteController@index')->name('cliente');
+
+/* Formulario de Registrar Cliente */
+Route::get('/clienteAdd', function(){
+    return view('clienteAdd');
+});
+Route::get('clienteAdd', 'ClienteController@create')->name('clienteAdd');
+
+
+/* Agregar Cliente */
+Route::post('/clienteAdd', 'ClienteController@post');
+/*---------------------------------------*/
