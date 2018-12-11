89662ac112c001ab2d0c83467e84c86953866aee
Diego <Diego_escorchi@hotmail.com>
2018-12-11 14:15:34 -0600

    Creando la Migrate "Proveedores"

---
+++ database/migrations/2018_12_11_182534_create_proveedores_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProveedoresTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('proveedores', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('nombre_proveedor',255);
+            $table->string('nombre_contacto',255);
+            $table->string('cargo_contacto',255);
+            $table->string('telefono',10);
+            $table->string('ciudad',255);
+            $table->string('pais',255);
+            $table->text('direccion');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('proveedores');
+    }
+}
