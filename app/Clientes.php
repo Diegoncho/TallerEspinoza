d84b9674eef27f7b9bbf72d2fb1e3e062990d84f
Diego <Diego_escorchi@hotmail.com>
2018-12-11 22:48:50 -0600

    Agregando Query de Busqueda

--- app/Clientes.php
+++ app/Clientes.php
@@ -13,4 +13,18 @@ class Clientes extends Model
         'id', 'nombres', 'apellidos', 'direccion', 'telefono', 'dui',
         'nit'
     ];
+
+    public function getFullNameAttribute()
+    {
+        return $this->nombres.' '.$this->apellidos;
+    }
+
+    public function scopeName($query, $name)
+    {
+        if (trim($name) != ""){
+
+            $query->where(\DB::raw("CONCAT(nombres, ' ' ,apellidos)"), "LIKE", "%$name%");
+        }
+
+    }
 }
