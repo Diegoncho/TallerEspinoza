eea8efdc86dcbbccfbc54f61ced10a6b6695cb6f
Diego <Diego_escorchi@hotmail.com>
2018-12-11 22:33:20 -0600

    Agregando Query de Busqueda

--- app/Empleados.php
+++ app/Empleados.php
@@ -13,4 +13,18 @@ class Empleados extends Model
         'fecha_nac', 'estado_civil', 'sexo', 'departamento_id', 'dui',
         'nit', 'afp', 'email', 'img'
     ];
+
+    public function getFullNameAttribute()
+    {
+        return $this->nombres.' '.$this->apellidos;
+    }
+
+    public function scopeName($query, $name)
+    {
+        if (trim($name) != ""){
+
+            $query->where(\DB::raw("CONCAT(nombres, ' ' ,apellidos)"), "LIKE", "%$name%");
+        }
+
+    }
 }
