cf534bca0bd24a006aa2ed6f979fb318ac242fdd
Diego <Diego_escorchi@hotmail.com>
2018-12-12 21:43:45 -0600

    Creación de nuevo "Model"

---
+++ app/VistaEmpleados.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class VistaEmpleados extends Model
+{
+    public $timestamps = false;
+
+    public $table = "vta_empleados";
+
+    protected $fillable = [
+        'id', 'nombres', 'apellidos', 'direccion', 'telefono', 'edad',
+        'fecha_nac', 'estado_civil', 'sexo', 'nombre', 'dui',
+        'nit', 'afp', 'email', 'img'
+    ];
+}
