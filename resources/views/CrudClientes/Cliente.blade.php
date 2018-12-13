73ea4699eb4b624c3be2380bf695d1ba9eb939bc
Diego <Diego_escorchi@hotmail.com>
2018-12-12 20:40:56 -0600

    Agregando mensaje de "Eliminar"

--- resources/views/CrudClientes/Cliente.blade.php
+++ resources/views/CrudClientes/Cliente.blade.php
@@ -7,6 +7,11 @@
     <div class="panel panel-default" style="min-width: 800px">
         <div class="panel-heading" style="background:#f9f9f9"><b>Listado de clientes</b></div>
         <div class="panel-body">
+            @if (Session::has('message'))
+                <div class="alert alert-success">
+                    <i class="icon-check_circle"></i> {{ Session::get('message') }}
+                </div>
+            @endif
 
             <form  action="{{ route('cliente') }}" class="navbar-form navbar-left pull-right" method="GET" role="search">
                 <div class="form-group">
@@ -31,7 +36,7 @@
 
             <tbody>
             @foreach($Clientes as $row)
-                <tr>
+                <tr data-id="{{ $row->id }}">
                     <td>{{ $row->id }}</td>
                     <td>{{ $row->nombres }}</td>
                     <td>{{ $row->apellidos }}</td>
@@ -39,7 +44,7 @@
                     <td>
                         <a href="" class="btn btn-info"><span class="icon-visibility"></span></a>
                         <a href="" class="btn btn-warning"><span class="icon-mode_edit"></span></a>
-                        <a href="" class="btn btn-danger"><span class="icon-highlight_off"></span></a>
+                        <a href="#" class="btn btn-danger"><span class="icon-highlight_off"></span></a>
                     </td>
                 </tr>
             @endforeach
@@ -49,5 +54,33 @@
     </div>
 
 {{ $Clientes->render() }}
+
+<form action="{{ route('cliente') }}/id" method="POST" id="form-delete">
+    {{ csrf_field() }}
+    {{ method_field('DELETE') }}                           
+</form>
+
+<script type="text/javascript">
+        $(document).ready(function (){
+            $('.btn-danger').click(function (e){
+                e.preventDefault();
+
+                var row = $(this).parents('tr');
+                var id = row.data('id');
+                var form = $('#form-delete');
+                var url = form.attr('action').replace('id', id);
+                var data = form.serialize();
+
+                row.fadeOut();
+
+                $.post(url, data, function(result){
+                }).fail(function (){
+                    alert('El registro no fue eliminado');
+                    row.show();
+                });
+                
+            });
+        });
+</script>
 
 @endsection
