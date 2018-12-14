bac2adc5d73a76d2d1b1651be4d7dae2e2ffc75e
Diego <Diego_escorchi@hotmail.com>
2018-12-13 22:13:53 -0600

    Agregnado Campo "departamento_id"

--- app/VistaEmpleados.php
+++ app/VistaEmpleados.php
@@ -12,7 +12,7 @@ class VistaEmpleados extends Model
 
     protected $fillable = [
         'id', 'nombres', 'apellidos', 'direccion', 'telefono', 'edad',
-        'fecha_nac', 'estado_civil', 'sexo', 'nombre', 'dui',
-        'nit', 'afp', 'email', 'img'
+        'fecha_nac', 'estado_civil', 'sexo', 'departamento_id', 'nombre', 
+        'dui', 'nit', 'afp', 'email', 'img'
     ];
 }
