03b181f4e6ef72093aba15988c46203da9496443
Diego <Diego_escorchi@hotmail.com>
2018-12-13 22:14:55 -0600

    Asignando todas las configuraciones para la ejecucion de "Actualizar"

--- app/Http/Controllers/EmpleadoController.php
+++ app/Http/Controllers/EmpleadoController.php
@@ -7,6 +7,7 @@ use Illuminate\Support\Facades\Validator;
 
 use App\Departamentos;
 use App\Empleados;
+use App\VistaEmpleados;
 
 class EmpleadoController extends Controller
 {
@@ -27,6 +28,15 @@ class EmpleadoController extends Controller
         $Departamentos = Departamentos::all();
 
         return view('CrudEmpleados.empleadoAdd', compact('Departamentos'));
+    }
+
+    public function edit($id){
+
+        $Empleados = Empleados::findOrFail($id);
+        $VistaEmpleados = VistaEmpleados::findOrFail($id);
+        $Departamentos = Departamentos::all();
+
+        return view('CrudEmpleados.empleadoEdit', compact('Empleados','VistaEmpleados','Departamentos'));
     }
 
     public function post(Request $request){
@@ -82,6 +92,89 @@ class EmpleadoController extends Controller
             $Empleados->save();
 
             return redirect('/empleado');
+    }
+
+    public function put(Request $request, $id){
+
+        $validator = Validator::make($request->all(),[
+            'nombres' => 'required|max:255',
+            'apellidos' => 'required|max:255',
+            'direccion' => 'required',
+            'telefono' => 'required|max:10',
+            'estado_civil' => 'required|max:255',
+            'fecha_nac' => 'required|date',
+            'edad' => 'required|max:5',
+            'sexo' => 'required',
+            'departamento_id' => 'required',
+            'dui' => 'required|max:20',
+            'nit' => 'required|max:20',
+            'afp' => 'required|max:255',
+            'email' => 'required|email',
+            'img' => 'image',
+        ]);
+
+        if ($validator->fails()) {
+            return redirect("/empleadoEdit/$id")
+            ->withInput()
+            ->withErrors($validator);
+        }
+
+        $file =$request->file('img');
+
+        if ($file != null){
+            
+            $file = $request->file('img');
+    
+            //obtenemos el nombre del archivo
+            $nombre = 'default'.time().'.'.$file->getClientOriginalExtension();
+
+            //indicamos que queremos guardar un nuevo archivo en el disco local
+            \Storage::disk('local')->put($nombre,  \File::get($file));
+
+            $Empleados = Empleados::findOrFail($id);
+
+            $Empleados->nombres = $request->nombres;
+            $Empleados->apellidos = $request->apellidos;
+            $Empleados->direccion = $request->direccion;
+            $Empleados->telefono = $request->telefono;
+            $Empleados->estado_civil = $request->estado_civil;
+            $Empleados->fecha_nac = $request->fecha_nac;
+            $Empleados->edad = $request->edad;
+            $Empleados->sexo = $request->sexo;
+            $Empleados->departamento_id = $request->departamento_id;
+            $Empleados->dui = $request->dui;
+            $Empleados->nit = $request->nit;
+            $Empleados->afp = $request->afp;
+            $Empleados->email = $request->email;
+            $Empleados->img = $nombre;
+
+            $Empleados->save();
+
+            return redirect('/empleado');
+        }
+
+        else{
+
+            $Empleados = Empleados::findOrFail($id);
+
+            $Empleados->nombres = $request->nombres;
+            $Empleados->apellidos = $request->apellidos;
+            $Empleados->direccion = $request->direccion;
+            $Empleados->telefono = $request->telefono;
+            $Empleados->estado_civil = $request->estado_civil;
+            $Empleados->fecha_nac = $request->fecha_nac;
+            $Empleados->edad = $request->edad;
+            $Empleados->sexo = $request->sexo;
+            $Empleados->departamento_id = $request->departamento_id;
+            $Empleados->dui = $request->dui;
+            $Empleados->nit = $request->nit;
+            $Empleados->afp = $request->afp;
+            $Empleados->email = $request->email;
+
+            $Empleados->save();
+
+            return redirect('/empleado');
+        }
     }
 
     public function delete($id){
