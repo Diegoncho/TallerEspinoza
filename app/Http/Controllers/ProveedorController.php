7b510f3282b86a0274ea1d492bcacf81c83c27de
Diego <Diego_escorchi@hotmail.com>
2018-12-14 14:56:02 -0600

    Asignando las Funciones de "Actualizar y Eliminar"

--- app/Http/Controllers/ProveedorController.php
+++ app/Http/Controllers/ProveedorController.php
@@ -13,15 +13,26 @@ class ProveedorController extends Controller
         $this->middleware('auth');
     }
 
-    public function index(){
+    public function index(Request $request){
 
-        $Proveedores = Proveedores::orderby('id','ASC')->paginate(1);
+        $Proveedores = Proveedores::name($request->get('name'))->orderby('id','ASC')->paginate(7);
 
-        return view('CrudProveedores.Proveedores' , compact('Proveedores'));
+        return view('CrudProveedores.proveedor' , compact('Proveedores'));
     }
 
+    public function create(){
 
-    public function post(Request $request){
+        return view('CrudProveedores.proveedorAdd');
+    }
+
+    public function edit($id){
+
+        $Proveedores = Proveedores::findOrFail($id);
+
+        return view('CrudProveedores.proveedorEdit', compact('Proveedores'));
+    }
+
+    public function post(Request $request){ 
 
         $validator = Validator::make($request->all(),[
             'nombre_proveedor' => 'required|max:255',
@@ -51,6 +62,50 @@ class ProveedorController extends Controller
 
             $Proveedores->save();
 
-            return redirect('/menu');
+            return redirect('/proveedor');
+    }
+
+    public function put(Request $request, $id){
+
+        $validator = Validator::make($request->all(),[
+            'nombre_proveedor' => 'required|max:255',
+            'nombre_contacto' => 'required|max:255',
+            'cargo_contacto' => 'required|max:255',
+            'telefono' => 'required|max:10',
+            'ciudad' => 'required|max:255',
+            'pais' => 'required|max:255',
+            'direccion' => 'required',
+        ]);
+
+        if ($validator->fails()) {
+            return redirect("/proveedorEdit/$id")
+            ->withInput()
+            ->withErrors($validator);
+        }
+
+            $Proveedores = Proveedores::findOrFail($id);
+
+            $Proveedores->nombre_proveedor = $request->nombre_proveedor;
+            $Proveedores->nombre_contacto = $request->nombre_contacto;
+            $Proveedores->cargo_contacto = $request->cargo_contacto;
+            $Proveedores->telefono = $request->telefono;
+            $Proveedores->ciudad = $request->ciudad;
+            $Proveedores->pais = $request->pais;
+            $Proveedores->direccion = $request->direccion;
+
+            $Proveedores->save();
+
+            return redirect('/proveedor');
+    }
+
+    public function delete($id){
+
+        $Proveedores = Proveedores::findOrFail($id);
+
+        $Proveedores->delete();
+
+        \Session::flash('message', $Proveedores->nombre_proveedor.' Fue eliminado.');
+    
+        return redirect('/proveedor');
     }
 }
