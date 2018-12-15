dcfa200d01343c67c56083072ba863d0c78a828b
Diego <Diego_escorchi@hotmail.com>
2018-12-15 12:57:19 -0600

    Corrigiendo view "Vehiculo"

--- resources/views/CrudVehiculos/Vehiculo.blade.php
+++ resources/views/CrudVehiculos/Vehiculo.blade.php
@@ -7,43 +7,46 @@
     <div class="panel panel-default" style="min-width: 800px">
         <div class="panel-heading" style="background:#f9f9f9"><b>Listado de vehiculos</b></div>
         <div class="panel-body">
+            @if (Session::has('message'))
+                <div class="alert alert-success">
+                    <i class="icon-check_circle"></i> {{ Session::get('message') }}
+                </div>
+            @endif
 
             <form  action="{{ route('vehiculo') }}" class="navbar-form navbar-left pull-right" method="GET" role="search">
                 <div class="form-group">
-                    <input type="text" class="form-control" name="name" placeholder="Marca del vehiculo">
+                    <input type="text" class="form-control" name="name" placeholder="Marca de vehiculo">
                 </div>
                 <button type="submit" class="btn btn-default">Buscar</button>
             </form>
 
-            <a href="{{ route('vehiculoAdd') }}" class="btn btn-info">Registrar nuevo vehiculo</a>
+            <a href="{{ route('vehiculoAdd') }}" class="btn btn-primary">Registrar nuevo vehiculo</a>
             
-            <p>Hay {{ $Empleados->total() }} vehiculos</p>
+            <p>Hay {{ $VistaVehiculos->total() }} vehiculos</p>
             <hr>
 
             <table class="table table-striped">
             <thead>
                 <th>#</th>
                 <th>Marca</th>
-                <th>Tipo de Vehiculo</th>
-                <th>Capacidad</th>
-                <th>Numero de Placa</th>
+                <th>Modelo</th>
                 <th>Color</th>
+                <th>Año</th>
                 <th>Acción</th>
             </thead>
 
             <tbody>
-            @foreach($Vehiculos as $row)
-                <tr>
+            @foreach($VistaVehiculos as $row)
+                <tr data-id="{{ $row->id }}">
                     <td>{{ $row->id }}</td>
                     <td>{{ $row->marca }}</td>
-                    <td>{{ $row->tipo_vehiculo }}</td>
-                    <td>{{ $row->capacidad }}</td>
-                    <td>{{ $row->numero_placa }}</td>
+                    <td>{{ $row->modelo }}</td>
                     <td>{{ $row->color }}</td>
+                    <td>{{ $row->anio }}</td>
                     <td>
                         <a href="" class="btn btn-info"><span class="icon-visibility"></span></a>
-                        <a href="" class="btn btn-warning"><span class="icon-mode_edit"></span></a>
-                        <a href="" class="btn btn-danger"><span class="icon-highlight_off"></span></a>
+                        <a href="{{ route('vehiculoEdit', $row->id) }}" class="btn btn-warning"><span class="icon-mode_edit"></span></a>
+                        <a href="#" class="btn btn-danger"><span class="icon-highlight_off"></span></a>
                     </td>
                 </tr>
             @endforeach
@@ -52,6 +55,34 @@
         </div>
     </div>
 
-{{ $Vehiculos->render() }}
+{{ $VistaVehiculos->render() }}
+
+<form action="{{ route('vehiculo') }}/id" method="POST" id="form-delete">
+    {{ csrf_field() }}
+    {{ method_field('DELETE') }}                           
+</form>
+
+<script type="text/javascript">
+        $(document).ready(function (){
+            $('.btn-danger').click(function (e){
+                e.preventDefault();
+
+                var row = $(this).parents('tr');
+                var id = row.data('id');
+                var form = $('#form-delete');
+                var url = form.attr('action').replace('id', id);
+                var data = form.serialize();
+
+                row.fadeOut();
+
+                $.post(url, data, function(result){
+                }).fail(function (){
+                    alert('El registro no fue eliminado');
+                    row.show();
+                });
+                
+            });
+        });
+</script>
 
 @endsection
