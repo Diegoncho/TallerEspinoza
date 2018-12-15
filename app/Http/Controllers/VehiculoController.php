3244a302bfece395e4a8567388fac55236c11136
Diego <Diego_escorchi@hotmail.com>
2018-12-15 12:54:32 -0600

    Asignando funciones para "Borrar y Actualizar"

--- app/Http/Controllers/VehiculoController.php
+++ app/Http/Controllers/VehiculoController.php
@@ -6,7 +6,9 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
 
 use App\Vehiculos;
-use App\Empleados;
+use App\Vmarcas;
+use App\Modelos;
+use App\VistaVehiculos;
 
 class VehiculoController extends Controller
 {
@@ -17,28 +19,37 @@ class VehiculoController extends Controller
 
     public function index(Request $request){
 
-        $Vehiculos = Vehiculos::name($request->get('name'))->orderby('id','ASC')->paginate(7);
+        $VistaVehiculos = VistaVehiculos::name($request->get('name'))->orderby('id','ASC')->paginate(7);
 
-        return view('CrudVehiculos.vehiculo' , compact('Vehiculos'));
+        return view('CrudVehiculos.vehiculo' , compact('VistaVehiculos'));
     }
 
     public function create(){
         
-        $Empleados = Empleados::all();
+        $Vmarcas = Vmarcas::all();
+        $Modelos = Modelos::all();
 
-        return view('CrudVehiculos.vehiculoAdd', compact('Empleados'));
+        return view('CrudVehiculos.vehiculoAdd', compact('Vmarcas','Modelos'));
+    }
+
+    public function edit($id){
+
+        $Vehiculos = Vehiculos::findOrFail($id);
+        $VistaVehiculos = VistaVehiculos::findOrFail($id);
+        $Vmarcas = Vmarcas::all();
+        $Modelos = Modelos::all();
+
+        return view('CrudVehiculos.vehiculoEdit', compact('Vehiculos','VistaVehiculos','Vmarcas','Modelos'));
     }
 
     public function post(Request $request){
 
         $validator = Validator::make($request->all(),[
-            'tipo_vehiculo' => 'required|max:255',
-            'marca' => 'required|max:255',
-            'modelo' => 'required|max:255',
-            'numero_placa' => 'required|max:20',
-            'capacidad' => 'required|max:100',
-            'color' => 'required|max:100',
-            'empleado_id' => 'required',
+            'marca_id' => 'required',
+            'modelo_id' => 'required',
+            'color' => 'required|max:255',
+            'matricula' => 'required|max:255',
+            'anio' => 'required|digits:4',
         ]);
 
         if ($validator->fails()) {
@@ -49,16 +60,55 @@ class VehiculoController extends Controller
 
             $Vehiculos = new Vehiculos;
 
-            $Vehiculos->tipo_vehiculo = $request->tipo_vehiculo;
-            $Vehiculos->marca = $request->marca;
-            $Vehiculos->modelo = $request->modelo;
-            $Vehiculos->numero_placa = $request->numero_placa;
-            $Vehiculos->capacidad = $request->capacidad;
+            $Vehiculos->marca_id = $request->marca_id;
+            $Vehiculos->modelo_id = $request->modelo_id;
             $Vehiculos->color = $request->color;
-            $Vehiculos->empleado_id = $request->empleado_id;
+            $Vehiculos->matricula = $request->matricula;
+            $Vehiculos->anio = $request->anio;
 
             $Vehiculos->save();
 
             return redirect('/vehiculo');
+    }
+
+    public function put(Request $request, $id){
+
+        $validator = Validator::make($request->all(),[
+            'marca_id' => 'required',
+            'modelo_id' => 'required',
+            'color' => 'required|max:255',
+            'matricula' => 'required|max:255',
+            'anio' => 'required|max:10',
+        ]);
+
+        if ($validator->fails()) {
+            return redirect("/vehiculoEdit/$id")
+            ->withInput()
+            ->withErrors($validator);
+        }
+
+            $Vehiculos = Vehiculos::findOrFail($id);
+
+            $Vehiculos->marca_id = $request->marca_id;
+            $Vehiculos->modelo_id = $request->modelo_id;
+            $Vehiculos->color = $request->color;
+            $Vehiculos->matricula = $request->matricula;
+            $Vehiculos->anio = $request->anio;
+
+            $Vehiculos->save();
+
+            return redirect('/vehiculo');
+    }
+
+    public function delete($id){
+        
+        $Vehiculos = Vehiculos::findOrFail($id);
+        $VistaVehiculos = VistaVehiculos::findOrFail($id);
+
+        $Vehiculos->delete();
+
+        \Session::flash('message', $VistaVehiculos->modelo.' Fue eliminado.');
+
+        return redirect('/vehiculo');
     }
 }
