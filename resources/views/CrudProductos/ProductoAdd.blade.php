2a2b2ffc204eb50ed4acf441d0de0f300dd2f70b
Diego <Diego_escorchi@hotmail.com>
2018-12-15 12:55:59 -0600

    Corrigiendo view "Producto"

--- resources/views/CrudProductos/ProductoAdd.blade.php
+++ resources/views/CrudProductos/ProductoAdd.blade.php
@@ -96,7 +96,7 @@
                         <div class="form-group">
                             <div class="col-md-6 col-md-offset-4">
                                 <button type="submit" class="btn btn-primary btn-block">
-                                <i class="icon-add_circle"></i> Insertar
+                                    <i class="icon-add_circle"></i> Insertar
                                 </button>
                             </div>
                         </div>
