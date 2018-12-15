f4cd0b0c10acfaf903a74f2d327c00ee3c817359
Diego <Diego_escorchi@hotmail.com>
2018-12-14 21:53:34 -0600

    Asignando Configuraciones para "Borrar y Actualizar"

--- app/Http/Controllers/ProductoController.php
+++ app/Http/Controllers/ProductoController.php
@@ -6,36 +6,50 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
 
 use App\Productos;
+use App\Pmarcas;
+use App\VistaProductos;
 
 class ProductoController extends Controller
 {
+
     public function __construct(){
         $this->middleware('auth');
     }
 
-    public function index(){
+    public function index(Request $request){
 
-        $Productos = Productos::orderby('id','ASC')->paginate(1);
+        $VistaProductos = VistaProductos::name($request->get('name'))->orderby('id','ASC')->paginate(7);
 
-        return view('CrudProductos.producto' , compact('Productos'));
+        return view('CrudProductos.producto', compact('VistaProductos'));
     }
 
     public function create(){
 
-        return view('CrudProductos.productoAdd');
+        $Pmarcas = Pmarcas::all();
+
+        return view('CrudProductos.productoAdd', compact('Pmarcas'));
+    }
+
+    public function edit($id){
+
+        $Productos = Productos::findOrFail($id);
+        $VistaProductos = VistaProductos::findOrFail($id);
+        $Pmarcas = Pmarcas::all();
+
+        return view('CrudProductos.productoEdit', compact('Productos','VistaProductos','Pmarcas'));
     }
 
     public function post(Request $request){
 
         $validator = Validator::make($request->all(),[
-            'nombres' => 'required|max:255',
-            'marca' => 'required|max:255',
-            'descripcion' => 'required|max:255',
-            'condicion' => 'required|max:100',
-            'vl_precio_compra' => 'required',
-            'precio_unitario' => 'required',
-            'c_existencia' => 'required',
+            'nombre' => 'required|max:255',
+            'marca_id' => 'required',
+            'descripcion' => 'required|max:250',
             'estado' => 'required',
+            'cantidad' => 'required',
+            'precio_costo' => 'required',
+            'precio_mayoreo' => 'required',
+            'precio_regular' => 'required',
         ]);
 
         if ($validator->fails()) {
@@ -46,18 +60,63 @@ class ProductoController extends Controller
 
             $Productos = new Productos;
 
-            $Productos->nombres = $request->nombres;
-            $Productos->marca = $request->marca;
+            $Productos->nombre = $request->nombre;
+            $Productos->marca_id = $request->marca_id;
             $Productos->descripcion = $request->descripcion;
-            $Productos->condicion = $request->condicion;
-            $Productos->vl_precio_compra = $request->vl_precio_compra;
-            $Productos->precio_unitario = $request->precio_unitario;
-            $Productos->c_existencia = $request->c_existencia;
             $Productos->estado = $request->estado;
+            $Productos->cantidad = $request->cantidad;
+            $Productos->precio_costo = $request->precio_costo;
+            $Productos->precio_mayoreo = $request->precio_mayoreo;
+            $Productos->precio_regular = $request->precio_regular;
 
             $Productos->save();
 
             return redirect('/producto');
     }
 
+    public function put(Request $request, $id){
+
+        $validator = Validator::make($request->all(),[
+            'nombre' => 'required|max:255',
+            'marca_id' => 'required',
+            'descripcion' => 'required|max:250',
+            'estado' => 'required',
+            'cantidad' => 'required',
+            'precio_costo' => 'required',
+            'precio_mayoreo' => 'required',
+            'precio_regular' => 'required',
+        ]);
+
+        if ($validator->fails()) {
+            return redirect("/productoEdit/$id")
+            ->withInput()
+            ->withErrors($validator);
+        }
+
+            $Productos = Productos::findOrFail($id);
+
+            $Productos->nombre = $request->nombre;
+            $Productos->marca_id = $request->marca_id;
+            $Productos->descripcion = $request->descripcion;
+            $Productos->estado = $request->estado;
+            $Productos->cantidad = $request->cantidad;
+            $Productos->precio_costo = $request->precio_costo;
+            $Productos->precio_mayoreo = $request->precio_mayoreo;
+            $Productos->precio_regular = $request->precio_regular;
+
+            $Productos->save();
+
+            return redirect('/producto');
+    }
+
+    public function delete($id){
+        
+        $Productos = Productos::findOrFail($id);
+
+        $Productos->delete();
+
+        \Session::flash('message', $Productos->nombre.' Fue eliminado.');
+
+        return redirect('/producto');
+    }
 }
