e5e76f4a8d40cc7d07d01d576b39c0f07c0a1ad6
Diego <Diego_escorchi@hotmail.com>
2018-12-14 21:55:33 -0600

    Agregando parametros

--- resources/views/CrudProductos/Producto.blade.php
+++ resources/views/CrudProductos/Producto.blade.php
@@ -7,32 +7,48 @@
     <div class="panel panel-default" style="min-width: 800px">
         <div class="panel-heading" style="background:#f9f9f9"><b>Listado de productos</b></div>
         <div class="panel-body">
-            <a href="{{ route('productoAdd') }}" class="btn btn-info">Registrar nuevo producto</a>
+            @if (Session::has('message'))
+                <div class="alert alert-success">
+                    <i class="icon-check_circle"></i> {{ Session::get('message') }}
+                </div>
+            @endif
+
+            <form  action="{{ route('producto') }}" class="navbar-form navbar-left pull-right" method="GET" role="search">
+                <div class="form-group">
+                    <input type="text" class="form-control" name="name" placeholder="Nombre de producto">
+                </div>
+                <button type="submit" class="btn btn-default">Buscar</button>
+            </form>
+
+            <a href="{{ route('productoAdd') }}" class="btn btn-primary">Registrar nuevo producto</a>
+
+            <p>Hay {{ $VistaProductos->total() }} productos</p>
             <hr>
+
             <table class="table table-striped">
             <thead>
-                <th>ID</th>
-                <th>Nombres</th>
-                <th>marca</th>
-                <th>condicion</th>
-                <th>precio</th>
-                <th>existencia</th>
+                <th>#</th>
+                <th>Nombre</th>
+                <th>Marca</th>
+                <th>Estado</th>
+                <th>Cantidad</th>
+                <th>Precio Costo</th>
                 <th>Acción</th>
             </thead>
 
             <tbody>
-            @foreach($Productos as $row)
-                <tr>
+            @foreach($VistaProductos as $row)
+                <tr data-id="{{ $row->id }}">
                     <td>{{ $row->id }}</td>
-                    <td>{{ $row->nombres }}</td>
+                    <td>{{ $row->nombre }}</td>
                     <td>{{ $row->marca }}</td>
-                    <td>{{ $row->condicion }}</td>
-                    <td>{{ $row->precio }}</td>
-                    <td>{{ $row->existencia }}</td>
+                    <td>{{ $row->estado }}</td>
+                    <td>{{ $row->cantidad }}</td>
+                    <td>${{ $row->precio_costo }}</td>
                     <td>
                         <a href="" class="btn btn-info"><span class="icon-visibility"></span></a>
-                        <a href="" class="btn btn-warning"><span class="icon-mode_edit"></span></a>
-                        <a href="" class="btn btn-danger"><span class="icon-highlight_off"></span></a>
+                        <a href="{{ route('productoEdit', $row->id) }}" class="btn btn-warning"><span class="icon-mode_edit"></span></a>
+                        <a href="#" class="btn btn-danger"><span class="icon-highlight_off"></span></a>
                     </td>
                 </tr>
             @endforeach
@@ -41,6 +57,34 @@
         </div>
     </div>
 
-{{ $Productos->render() }}
+{{ $VistaProductos->render() }}
+
+<form action="{{ route('producto') }}/id" method="POST" id="form-delete">
+    {{ csrf_field() }}
+    {{ method_field('DELETE') }}                           
+</form>
+
+<script type="text/javascript">
+        $(document).ready(function (){
+            $('.btn-danger').click(function (e){
+                e.preventDefault();
+
+                var row = $(this).parents('tr');
+                var id = row.data('id');
+                var form = $('#form-delete');
+                var url = form.attr('action').replace('id', id);
+                var data = form.serialize();
+
+                row.fadeOut();
+
+                $.post(url, data, function(result){
+                }).fail(function (){
+                    alert('El registro no fue eliminado');
+                    row.show();
+                });
+                
+            });
+        });
+</script>
 
 @endsection
