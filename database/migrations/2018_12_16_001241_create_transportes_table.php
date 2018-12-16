8bfc53bd61c6818c97c343f7c7060d6873764f38
Diego <Diego_escorchi@hotmail.com>
2018-12-15 21:18:47 -0600

    Creando Migrate "Transportes"

---
+++ database/migrations/2018_12_16_001241_create_transportes_table.php
@@ -0,0 +1,47 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTransportesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('transportes', function (Blueprint $table) {
+            $table->increments('id');
+            $table->date('fecha_inicio');
+            $table->date('fecha_fin');
+            $table->string('destino',255);
+            $table->integer('empleado_id')->unsigned();
+            $table->integer('vehiculo_id')->unsigned();
+            $table->string('carga',255);
+            $table->timestamps();
+
+            //Relaciones
+
+            $table->foreign('empleado_id')->references('id')->on('empleados')
+                ->onDelete('cascade')
+                ->onUpdate('cascade');
+            
+            $table->foreign('vehiculo_id')->references('id')->on('vehiculos')
+                ->onDelete('cascade')
+                ->onUpdate('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('transportes');
+    }
+}
