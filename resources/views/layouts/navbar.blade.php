f2ba65c8f497c0af959ccde955150ab776634d9b
Diego <Diego_escorchi@hotmail.com>
2018-12-16 19:54:28 -0600

    Ordenando "Scripts" y agregando "Datepicker"

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -1,8 +1,19 @@
-
+<!-- jquery Principal-->
 <script src="//code.jquery.com/jquery-2.1.4.min.js"></script>
+
+<!-- jquery para Sweetalert -->
 <script src="{{ asset('sweetalert/sweetalert.min.js') }}"></script>
+
+<!-- jquery para Datepicker -->
+<script src="https://code.jquery.com/jquery-1.12.4.js"></script>
+<script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+<link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
+
+<!-- jquery para LightBox -->
 <script src="{{ asset('ligthbox/lightbox.js') }}"></script>
 <link rel="stylesheet" href="{{ asset('ligthbox/lightbox.css') }}">
+
+
 
 <div class="menu-application flexbox">
         <div class="auth flex">
