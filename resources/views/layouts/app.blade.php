89d69d8b838507126f207b166fadc27631ecc99f
Diego Castro <Diego_escorchi@hotmail.com>
2019-01-11 21:51:07 -0600

    Añadiendo Scripts "Riot" y "EasyAutocomplete"

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,6 +5,7 @@
     <title>Taller</title>
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
     <link rel="stylesheet" href="{{ asset('fonts/style.css') }}">
+    <link rel="stylesheet" href="{{ asset('bower_components/EasyAutocomplete/dist/easy-autocomplete.min.css') }}">
 </head>
 <body>
     <div class="container">
@@ -19,14 +20,16 @@
 <script src="//code.jquery.com/jquery-2.1.4.min.js"></script>
 
 <!-- Script para Riot -->
-<script type="riot/tag" src="{{ asset('bower_components/riot/riot.min.js') }}"></script>
-<script type="riot/tag" src="{{ asset('bower_components/riot/riot+compiler.min.js') }}"></script>
+<script src="{{ asset('bower_components/riot/riot.min.js') }}"></script>
+<script src="https://cdn.jsdelivr.net/npm/riot@3.7/riot+compiler.min.js"></script>
+
+<!-- Script para EasyAutocomplete -->
+<script src="{{ asset('bower_components/EasyAutocomplete/dist/jquery.easy-autocomplete.min.js') }}"></script>
 
 <!-- jquery para Sweetalert -->
 <script src="{{ asset('sweetalert/sweetalert.min.js') }}"></script>
 
 <!-- jquery para Datepicker -->
-<script src="https://code.jquery.com/jquery-1.12.4.js"></script>
 <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
 <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
 
@@ -38,5 +41,16 @@
 <script src="https://unpkg.com/jspdf@latest/dist/jspdf.min.js"></script>
 <script src="{{ asset('js/html2canvas.js') }}"></script>
 
+<!-- jquery para Masked-Input -->
+<script src="{{ asset('js/jquery.maskedinput.min.js') }}"></script>
+
 @yield('scripts')
+@yield('navbar-script')
+
+<script type="text/javascript">
+    function baseUrl(url){
+        return '{{ url('') }}/' + url;
+    }
+</script>
+
 </html>
