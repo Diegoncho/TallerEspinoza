8943e05bb3d9a2a7120059488d096f3161fcae4b
Diego Castro <Diego_escorchi@hotmail.com>
2019-01-11 21:48:27 -0600

    Creando Migrate "Comprobantes"

---
+++ database/migrations/2019_01_10_224309_create_comprobantes_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateComprobantesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comprobantes', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('fecha',255);
+            $table->integer('cliente_id')->unsigned();
+            $table->decimal('iva', 10,2);
+            $table->decimal('subtotal', 10,2);
+            $table->decimal('total', 10,2);
+            $table->timestamps();
+
+            //Relaciones
+
+            $table->foreign('cliente_id')->references('id')->on('clientes')
+                ->onDelete('cascade')
+                ->onUpdate('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('comprobantes');
+    }
+}
