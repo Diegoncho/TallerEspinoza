2ec952546edb70d1a409b905b8c605719db04178
Diego Castro <Diego_escorchi@hotmail.com>
2019-01-11 21:51:45 -0600

    Creando View "FacturaAdd"

---
+++ resources/views/CrudFacturas/FacturaAdd.blade.php
@@ -0,0 +1,31 @@
+@extends('layouts.app')
+
+@section('content')
+
+@include('layouts.navbar')
+
+    <div class="row">
+        <div class="col-md-12">
+            <div class="panel panel-default" style="min-width: 830px">
+                <div class="panel-heading">
+                    Nuevo comprobante
+                </div>
+
+                <div class="panel-body">
+                    <invoice></invoice>
+                </div>
+            </div>
+        </div>
+    </div>
+
+@section('scripts')
+<script type="riot/tag" src="{{ asset('components/invoice.tag') }}"></script>
+
+    <script type="text/javascript">
+        $(document).ready(function(){
+            riot.mount('invoice');
+        });
+    </script>
+@endsection
+
+@endsection
