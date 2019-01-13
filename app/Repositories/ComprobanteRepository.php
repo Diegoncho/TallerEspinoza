3effd835a79de03f67c9abd7f3a7cd629da3b560
Diego Castro <Diego_escorchi@hotmail.com>
2019-01-12 21:14:01 -0600

    Creando Repository "Comprobante"

---
+++ app/Repositories/ComprobanteRepository.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Comprobantes;
+use App\ComprobanteDetalle;
+use DB;
+
+class ComprobanteRepository 
+{
+    private $model;
+
+    public function __construct() {
+
+        $this->model = new Comprobantes();
+    }
+
+    public function get($id) {
+        
+        return $this->model->find($id);
+    }
+
+    public function getAll(){
+
+        return $this->model->orderBy('id', 'desc')->get();
+    }
+
+    public function save($data){
+        $return = (object)[
+            'response' => false
+        ];
+
+        try {
+            DB::beginTransaction();
+
+            $this->model->iva = $data->iva;
+            $this->model->subtotal = $data->subtotal;
+            $this->model->total = $data->total;
+            $this->model->cliente_id = $data->cliente_id;
+
+            $this->model->save();
+
+            $detail = [];
+            foreach($data->detail as $d) {
+                $obj = new ComprobanteDetalle;
+
+                $obj->producto_id = $d->producto_id;
+                $obj->cantidad = $d->cantidad;
+                $obj->precio_unitario = $d->precio_unitario;
+                $obj->total = $d->total;
+
+                $detail[] = $obj;
+            }
+
+            $this->model->detail()->saveMany($detail);
+            $return->response = true;
+
+            DB::commit();
+        } catch (\Exception $e){
+            DB::rollBack();
+        }
+
+        return json_encode($return);
+    }
+}
