bce95a1c52c174997b84964eefcfa501c95fe59d
Diego Castro <Diego_escorchi@hotmail.com>
2019-01-13 21:08:38 -0600

    Creando View "CompraAdd"

--- resources/views/CrudCompras/CompraAdd.blade.php
+++ resources/views/CrudCompras/CompraAdd.blade.php
@@ -5,27 +5,108 @@
 @include('layouts.navbar')
 
     <div class="row">
-        <div class="col-md-12">
-            <div class="panel panel-default" style="min-width: 830px">
-                <div class="panel-heading">
-                    Nuevo comprobante
-                </div>
+        <div class="col-md-8 col-md-offset-2">
+            <div class="panel panel-default">
+                <div class="panel-heading">Insertar Compra</div>
 
-                <div class="panel-body">
-                    <invoice></invoice>
+                    <div class="panel-body">
+                        <form class="form-horizontal" action="{{ route('CompraAdd') }}" method="POST" autocomplete="off">
+                        {{ csrf_field() }}
+
+                        <div class="form-group {{ $errors->has('fecha_recibido') ? 'has-error' : ''}}">
+                            <div class="posting-read">Información de Compra <i class="icon-info"></i></div>
+
+                            <label for="fecha_recibido" class="col-md-4 control-label">Fecha de compra</label>
+
+                            <div class="col-md-6">
+                                <input id="fecha_recibido" readonly type="text" class="form-control" name="fecha" value="{{ old('fecha') }}">
+                                {!! $errors->first('fecha','<span class="help-block">:message</span>') !!}
+                            </div>
+                        </div>
+
+                        <div class="form-group {{ $errors->has('proveedor_id') ? 'has-error' : ''}}">
+                            <div class="posting-read">Proveedor del producto <i class="icon-person"></i></div>
+
+                            <label for="Proveedor_id" class="col-md-4 control-label">Proveedor</label>
+
+                            <div class="col-md-6">
+                                <select name="proveedor_id" id="proveedor_id" class="form-control selectpicker" data-show-subtext="true" data-live-search="true">       
+                                    <option Selected disabled>Seleccione el Proveedor</option>
+                                    
+                                @foreach($Proveedores as $row)
+                                    <option value="{{ $row->id }}">{{ $row->nombre_proveedor }} </option>
+                                @endforeach
+                                </select>
+                                {!! $errors->first('proveedor_id','<span class="help-block">:message</span>') !!}
+                            </div>
+                        </div>
+
+                        <div class="form-group {{ $errors->has('cantidad') ? 'has-error' : ''}}">
+                            <label for="cantidad" class="col-md-4 control-label">cantidad</label>
+
+                            <div class="col-md-6">
+                                <input id="cantidad" type="number" min="0" step="1" class="form-control" name="cantidad" value="{{ old('cantidad') }}">
+                                {!! $errors->first('cantidad','<span class="help-block">:message</span>') !!}
+                            </div>
+                        </div>
+
+                         <div class="form-group {{ $errors->has('descuento') ? 'has-error' : ''}}">
+                            <label for="descuento" class="col-md-4 control-label">Descuento</label>
+
+                            <div class="col-md-6">
+                                <input id="descuento" type="number" min="0.00" step="0.01" class="form-control" name="descuento" value="{{ old('descuento') }}">                              
+                                {!! $errors->first('descuento','<span class="help-block">:message</span>') !!}
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <div class="col-md-6 col-md-offset-4">
+                                <button type="submit" class="btn btn-primary btn-block">
+                                    <i class="icon-add_circle"></i> Insertar
+                                </button>
+                            </div>
+                        </div>
+
+                        </form>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
 
 @section('scripts')
-<script type="riot/tag" src="{{ asset('components/invoice.tag') }}"></script>
-
-    <script type="text/javascript">
-        $(document).ready(function(){
-            riot.mount('invoice');
+<script type="text/javascript">
+    $( function() {
+        var dateFormat = "mm/dd/yy",
+        from = $( "#fecha_recibido" )
+            .datepicker({
+            defaultDate: "+1w",
+            changeMonth: true,
+            numberOfMonths: 3
+            })
+            .on( "change", function() {
+            to.datepicker( "option", "minDate", getDate( this ) );
+            }),
+        to = $( "#fecha_entrega" ).datepicker({
+            defaultDate: "+1w",
+            changeMonth: true,
+            numberOfMonths: 3
+        })
+        .on( "change", function() {
+            from.datepicker( "option", "maxDate", getDate( this ) );
         });
-    </script>
+
+        function getDate( element ) {
+        var date;
+        try {
+            date = $.datepicker.parseDate( dateFormat, element.value );
+        } catch( error ) {
+            date = null;
+        }
+
+        return date;
+        }
+    });
+</script>
 @endsection
 
 @endsection
