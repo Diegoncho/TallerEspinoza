b766ebb7f3dc14f4da123a7dde8cbe254ad6c8cc
Diego Castro <Diego_escorchi@hotmail.com>
2019-01-13 22:42:42 -0600

    Creando Migrate "Compras"

---
+++ database/migrations/2019_01_13_212623_create_compras_table.php
@@ -0,0 +1,48 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateComprasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('compras', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('proveedor_id')->unsigned();
+            $table->string('fecha',255);
+            $table->integer('producto_id')->unsigned();
+            $table->integer('cantidad');
+            $table->decimal('subtotal', 10,2);
+            $table->decimal('descuento', 10,2);
+            $table->decimal('total', 10,2);
+            $table->timestamps();
+
+            //Relaciones
+
+            $table->foreign('proveedor_id')->references('id')->on('proveedores')
+                    ->onDelete('cascade')
+                    ->onUpdate('cascade');
+        
+            $table->foreign('producto_id')->references('id')->on('productos')
+                    ->onDelete('cascade')
+                    ->onUpdate('cascade');
+        });
+    } 
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('compras');
+    }
+}
